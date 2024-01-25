42625f1d510b96e993a912bec982e44cc00b5cae
Anastasiya Sachko <baron-nastya2009@yandex.ru>
2024-01-26 00:34:45 +0300

    feat: implement fs/create.js solution

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,24 @@
+import fs from 'node:fs/promises';
+import { existsSync } from 'node:fs';
+import { fileURLToPath } from 'url';
+import path from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const create = async () => {
-    // Write your code here 
+  const content = 'I am fresh and young';
+  const filePath = path.resolve(__dirname, './files/fresh.txt');
+
+  if (existsSync(filePath)) {
+    throw new Error('FS operation failed');
+  }
+
+  try {
+    await fs.writeFile(filePath, content);
+  } catch (error) {
+    throw new Error('FS operation failed');
+  }
 };
 
 await create();
