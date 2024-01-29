f091d4a17c3f2c5535903c88875b4967ea5ec06a
Anastasiya Sachko <baron-nastya2009@yandex.ru>
2024-01-29 22:38:03 +0300

    feat: implement hash solution

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,16 @@
+import fs from 'node:fs/promises';
+import { createHash } from 'node:crypto';
+import { fileURLToPath } from 'url';
+import path from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const calculateHash = async () => {
-    // Write your code here 
+  const file = path.join(__dirname, 'files', 'fileToCalculateHashFor.txt');
+  const fileBuffer = await fs.readFile(file);
+  const hex = createHash('SHA256').update(fileBuffer).digest('hex');
+  console.log(hex);
 };
 
 await calculateHash();
