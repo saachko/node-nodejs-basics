310549e6d0cd7ec3fc23fb2d749726b5083c5c1d
Anastasiya Sachko <baron-nastya2009@yandex.ru>
2024-01-29 19:39:10 +0300

    feat: implement cli/args.js solution

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,18 @@
 const parseArgs = () => {
-    // Write your code here 
+  const myArgs = process.argv.slice(2);
+
+  const result = myArgs
+    .reduce((acc, arg, index) => {
+      if (index % 2 === 0) {
+        const propName = arg.slice(2);
+        const propValue = myArgs[index + 1];
+        acc.push(`${propName} is ${propValue}`);
+      }
+      return acc;
+    }, [])
+    .join(', ');
+
+  console.log(result);
 };
 
 parseArgs();
